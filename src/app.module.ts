64d13d3c7cec16e6c5e37f822760866ca138e0c3
elpwc/wniko <elpwc@hotmail.com>
2021-12-16 00:52:46 +0800

    typeorm config done

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,22 +8,13 @@ import { BlogSubjectsModule } from './blog-subjects/blog-subjects.module';
 import { RepliesModule } from './replies/replies.module';
 import { VisitorsModule } from './visitors/visitors.module';
 import { IllustsModule } from './illusts/illusts.module';
-
-import { ProjectEntity } from './projects/entities/project.entity';
-
-import { dbConfig } from './dbconfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
-    TypeOrmModule.forRoot({
-      ...dbConfig,
-      models: [ProjectEntity],
-      autoLoadModels: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     ProjectsModule,
     BlogsModule,
     BlogCommentsModule,
